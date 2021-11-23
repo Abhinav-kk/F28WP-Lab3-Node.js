ebfeabd9ae4ee083b57027ce32ba19f1d374ce49
Abhinav-kk <akk2002@hw.ac.uk>
2021-11-23 09:19:24 +0400

    Updated

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const express = require('express');
 //creating app
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const session = require('express-session');
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 } }))
+
 app.use(express.static(__dirname + '/public'));
 app.get('/',(req,res) => {
     res.sendFile("public/index.html",{root: __dirname});
@@ -32,6 +37,7 @@ app.use(router);
 //handling static HTML and EJS templates
 app.use(express.static('public'));
 app.set('view engine','ejs');
+
 app.get('/',(req,res) => {
     res.render('index');
 })
@@ -41,14 +47,7 @@ app.get('/contacts',(req,res) => {
     res.render('contacts');
 });
 
-app.get('/login',(req,res) => {
-    res.render('login');
-});
-
-app.get('/register',(req,res) => {
-    res.render('register');
-});
-
+//route for catalog
 app.get('/api/catalog',(req,res) =>{
     res.render('catalog')
 });
